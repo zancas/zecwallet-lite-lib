cc0d41ad7ae700225ede62925565533d55a68393
Za Wilcox <zancas@protonmail.com>
2019-10-20 16:30:52 -0600

    run a configuration in test code

--- tests/startup_failures.rs
+++ tests/startup_failures.rs
@@ -2,5 +2,8 @@ extern crate zecwalletlitelib;
 
 #[test]
 fn unauthorized_user_dotzcash_file_access() {
+    use clap::{App, Arg};
+    use zecwalletlitelib::{configure_clapapp, lightclient};
+    configure_clapapp!(App::new("Zecwallet CLI Test Unauthorized User"));
     assert_eq!(2 + 2, 4);
 }
